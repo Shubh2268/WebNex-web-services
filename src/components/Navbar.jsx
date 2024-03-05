67f9331104d566c6a41183a7c329f96a674bc1be
Shubh <shubh2268@gmail.com>
2024-03-05 20:03:59 +0530

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LiaTimesSolid } from 'react-icons/lia';
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
 
   const links = [
     {
@@ -26,7 +28,7 @@ const Navbar = () => {
   return (
     <div className='w-full z-20 fixed'>
 
-      <div className='px-5 flex items-center justify-between w-full h-full'>
+      <div className='px-10 flex items-center justify-between w-full h-full'>
 
         <div className='flex items-center'>
           <h1 className='text-2xl sm:text-3xl text-primary font-bold py-2 sm:py-0'>WebNex</h1>
@@ -49,15 +51,45 @@ const Navbar = () => {
 
         {/* burger icon */}
         <div className='sm:hidden'>
-          <div className='cursor-pointer text-primary'>
-            <HiOutlineMenuAlt2 onClick={() => setNavigation(true)} size={25} />
+          <div className='cursor-pointer'>
+            <HiOutlineMenuAlt2 onClick={() => setNav(true)} size={25} />
           </div>
         </div>
 
       </div>
 
       {/* mobile menu  */}
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-full bg-gray-200 backdrop-blur' : ''}>
+        <div className={nav ? 'fixed left-0 top-0 w-4/5 h-full p-10 ease-in duration-500' : 'fixed top-0 left-[-100%] h-full p-10 duration-500 ease-in'}>
+          <div>
+            <div className='flex w-full items-center justify-between'>
+              <h2 onClick={() => setNav(false)} className='text-2xl text-primary font-bold capitalize cursor-pointer'>WebNex</h2>
+              <div className='p-3 cursor-pointer'>
+                <LiaTimesSolid onClick={() => setNav(false)} size={25} />
+              </div>
+            </div>
+          </div>
 
+          <div className='mt-6 flex flex-col items-start h-fit gap-12'>
+
+            <ul className='capitalize'>
+              {
+                links.map(({ id, link }) => (
+                  <li key={id} className='py-2 text-lg font-medium cursor-pointer hover:text-primary'>
+                    <a onClick={() => setNav(false)} duration={500} offset={-75}>
+                      {link}
+                    </a>
+                  </li>
+                ))
+              }
+              <div className='flex items-center'>
+                <button className='capitalize font-medium px-5 py-2 my-3 bg-primary text-white rounded-md' >contact us</button>
+              </div>
+            </ul>
+
+          </div>
+        </div>
+      </div>
 
     </div>
   )
