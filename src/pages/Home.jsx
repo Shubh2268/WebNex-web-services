a86c8a038063f65e055209d0ad52aab0dc76387a
Shubh <shubh2268@gmail.com>
2024-03-06 17:50:47 +0530

    --

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,26 @@ import BannerImg from '../assets/Banner.png';
 
 const Home = () => {
   return (
-    <div className='grid md:grid-cols-2 h-full p-10 bg-primary/10'>
-      <div className='flex flex-col justify-center w-10/12 px-10 ml-10'>
-        <h2 className='font-semibold text-4xl'>Experienced <span className='text-primary'>mobile and web</span> applications and website builders measuring.</h2>
-        <p className='py-7 text-[#5C5C5C]'>WebNex TECHNOLOGY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. </p>
-        <div className='flex mt-7 items-start'>
-          <button className='capitalize mx-1 px-8 py-2 bg-primary text-white rounded-md'>contact us</button>
-          <button className='capitalize mx-1 px-8 py-2 bg-white text-primary rounded-md border-[1px] border-primary'>view more</button>
+    <>
+      <div className='grid md:grid-cols-2 h-full p-10 bg-primary/10'>
+        <div className='flex flex-col justify-center w-10/12 px-10 ml-10'>
+          <h2 className='font-semibold text-4xl'>Experienced <span className='text-primary'>mobile and web</span> applications and website builders measuring.</h2>
+          <p className='py-7 text-[#5C5C5C]'>WebNex TECHNOLOGY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. </p>
+          <div className='flex mt-7 items-start'>
+            <button className='capitalize mx-1 px-8 py-2 bg-primary text-white rounded-md'>contact us</button>
+            <button className='capitalize mx-1 px-8 py-2 bg-white text-primary rounded-md border-[1px] border-primary'>view more</button>
+          </div>
+        </div>
+
+        <div className='flex justify-end items-center w-4/5'>
+          <img src={BannerImg} alt='BannerImg' />
         </div>
       </div>
 
-      <div className='flex justify-end items-center w-4/5'>
-        <img src={BannerImg} alt='BannerImg' />
+      <div>
+          
       </div>
-    </div>
+    </>
   )
 }
 
