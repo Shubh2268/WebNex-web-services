e8a8e845a599ea4f181f1f970ec2fd4642563f29
Shubh <shubh2268@gmail.com>
2024-03-07 20:13:55 +0530

    --

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const RightCard = () => {
+const TextCard = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const RightCard = () => {
   )
 }
 
-export default RightCard
+export default TextCard
