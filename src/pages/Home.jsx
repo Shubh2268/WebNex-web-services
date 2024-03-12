0662954e929e21510ac4f3b3c5c1fb632bb62cf6
Shubh <shubh2268@gmail.com>
2024-03-12 20:36:39 +0530

    --

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
-// import TextCard from '../components/TextCard';
+import TextCard from '../components/TextCard';
+import homeImg1 from '../assets/homeImg1.png';
+import homeImg2 from '../assets/homeImg2.png';
+import homeImg3 from '../assets/homeImg3.png';
 
 const Home = () => {
   return (
@@ -11,7 +14,11 @@ const Home = () => {
         <h1 className='text-3xl font-semibold'>Lorem Ipsum is simply dummy text of the printing. </h1>
         <p className='text-[#868686] w-1/2 text-center py-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />Lorem Ipsum has been the industry's</p>
       </div>
-      {/* <TextCard className='flex flex-col justify-center items-center my-5' textClass='w-1/2' /> */}
+
+      <div className='flex'>
+        <TextCard className='flex flex-col m-10 p-20 w-1/2 ' textClass='py-5' />
+        <img src={homeImg1} alt="" className='w-3/5' />
+      </div>
     </div>
   )
 }
